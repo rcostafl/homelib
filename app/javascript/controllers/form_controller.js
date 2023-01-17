dbc3014273204f414613d1368b3c5d1426f33ac9
rafael costa <rcostafl@gmail.com>
2023-01-16 21:51:39 -0300

    list items being inserted

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,18 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
   
-  static targets = ["items", "form"]
+  static targets  = ["formtag","titletag", "searchList"]
 
   connect(event) {
-    //removeMarginFromFromInputs()
+    removeMarginFromFromInputs()
     //console.log(this.element)
     //console.log(this.itemsTarget)
     //console.log(this.formTarget)
-    console.log("entrei")
+    //console.log("entrei")
+    //console.log(this.element.action)
+    //console.log(this.formtagTarget)
+    //console.log(this.titletagTarget)
+    
   }
 
   showSearchList(event) {
-    alert('showSearchList')
+    const search_list_el = document.getElementById("search_list")
+
+    fetch('http://www.omdbapi.com/?s=godfather&apikey=adf1f2d7')
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        console.log(result.Title)
+        let li_el = document.createElement("li")
+        li_el.innerHTML = result.Title
+        search_list_el.appendChild(li_el)
+        
+      })
+    })
   }  
 }
 
